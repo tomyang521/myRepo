fb433f083e368b50c2f846de1d1b231dce451c57
Tom <Tomyang521@gmail.com>
2022-02-27 01:04:50 -0500

    react course 2

---
+++ react-course-2/src/App.js
@@ -0,0 +1,21 @@
+import {Routes, Route} from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+
+function App() {
+  //localhost:3000
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
